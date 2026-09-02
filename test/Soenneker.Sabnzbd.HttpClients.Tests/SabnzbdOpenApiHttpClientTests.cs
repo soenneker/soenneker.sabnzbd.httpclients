741d4382025ad993611139654f9ca1049a0fefb5
soenneker <jake@soenneker.com>
2026-09-02 14:22:08 -0500

    Add cancellation tokens to async tests

--- test/Soenneker.Sabnzbd.HttpClients.Tests/SabnzbdOpenApiHttpClientTests.cs
+++ test/Soenneker.Sabnzbd.HttpClients.Tests/SabnzbdOpenApiHttpClientTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Soenneker.Sabnzbd.HttpClients.Abstract;
 using Soenneker.Tests.HostedUnit;
@@ -17,9 +18,9 @@ public sealed class SabnzbdOpenApiHttpClientTests : HostedUnitTest
     }
 
     [Test]
-    public async Task Get_uses_configured_sabnzbd_base_url()
+    public async Task Get_uses_configured_sabnzbd_base_url(CancellationToken cancellationToken)
     {
-        HttpClient client = await _httpclient.Get();
+        HttpClient client = await _httpclient.Get(cancellationToken: cancellationToken);
 
         await Assert.That(client.BaseAddress).IsEqualTo(new Uri("http://localhost:18080"));
         await Assert.That(client.DefaultRequestHeaders.Authorization).IsNull();
